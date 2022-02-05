5f34114711b63e9db83eb061dda45e1fed6090e7
Danilo Gutesa <danilo.gutesa99@gmail.com>
2022-02-05 13:52:17 +0100

    Gradovi iz baze dobijeni preko axios get zahteva

--- src/components/FirstPage.js
+++ src/components/FirstPage.js
@@ -1,9 +1,24 @@
 import FormAddCity from "./FormAddCity";
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+
 
 function FirstPage() {
+
+    const [gradovi, setGradovi] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/grad').then(odgovor => {
+            setGradovi(odgovor.data.data);
+        })
+    }, []);
+
+    console.log(gradovi)
+
     return (
         <div className="first-page">
-            <FormAddCity />
+            <FormAddCity gradovi={gradovi} />
         </div>
     );
 }
