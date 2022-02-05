e0bcbb468fcb2534671344333c79de39e24a796b
Danilo Gutesa <danilo.gutesa99@gmail.com>
2022-02-05 15:50:16 +0100

    Forma za dodavanje novog grada, funkcija za dodavanje grada u niz

--- src/components/FirstPage.js
+++ src/components/FirstPage.js
@@ -8,19 +8,24 @@ import Nav from './Nav'
 function FirstPage() {
 
     const [gradovi, setGradovi] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         axios.get('http://localhost:8000/api/grad').then(odgovor => {
             setGradovi(odgovor.data.data);
+            setLoading(false);
         })
     }, []);
 
     console.log(gradovi)
+    if (loading)
+        return <h1>Loading...</h1>
+
 
     return (
         <div className="first-page">
             <Nav />
-            <h2>Forma za dodavanje novog grada</h2>
+            <h2 id="nova-forma-h2">Forma za dodavanje novog grada</h2>
             <FormAddCity gradovi={gradovi} />
         </div>
     );
--- src/components/FormAddCity.js
+++ src/components/FormAddCity.js
@@ -1,8 +1,37 @@
+import { useState } from 'react'
 
+function FormAddCity(props) {
 
-function FormAddCity() {
+    const [gradovi2, setGradovi2] = useState(props.gradovi);
+
+    const [grad, setGrad] = useState({
+        ime: '',
+        drzava: '',
+        brojStanovnika: '',
+    })
+
+    console.log(gradovi2)
+
+    function noviGrad(grad) {
+        setGradovi2(gradovi2 => [...gradovi2, grad])
+        alert('Uspešno dodat grad ' + grad.ime + '!')
+    }
+
     return (
         <div className="form-add-city">
+            <div className="add-city-fields">
+                <label>Ime: </label>
+                <input type={'text'} className="form-control" value={grad.ime} onChange={e => setGrad({ ...grad, ime: e.target.value })} />
+            </div>
+            <div className="add-city-fields">
+                <label>Država: </label>
+                <input type={'text'} className="form-control" value={grad.drzava} onChange={e => setGrad({ ...grad, drzava: e.target.value })} />
+            </div>
+            <div className="add-city-fields">
+                <label>Broj stanovnika: </label>
+                <input type={'number'} className="form-control" value={grad.brojStanovnika} onChange={e => setGrad({ ...grad, brojStanovnika: e.target.value })} />
+            </div>
+            <button type="button" onClick={() => noviGrad(grad)} className="btn btn-primary" id="dugme_novi">Dodaj</button>
         </div>
     );
 }
